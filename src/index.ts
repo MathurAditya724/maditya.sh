db9f45b241c32983926bf5b61b9541f663cd4f40
mathuraditya724 <mathuraditya724@gmail.com>
2025-10-31 01:37:02 +0530

    feat: added cache middleware and updated the config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Hono } from "hono";
+import { cache } from "hono/cache";
 import { renderer } from "./renderer";
 import routes from "./routes";
 
-const app = new Hono();
+const app = new Hono().use(
+  cache({
+    cacheName: "maditya-sh",
+    cacheControl: "max-age=3600",
+  }),
+);
 
 app.use(renderer);
 
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -4,7 +4,10 @@ const router = new Hono();
 
 router.get("/", (c) => {
   return c.render(
-    <p class="text-base leading-[1.7] text-gray-300">Building random stuff.</p>,
+    <p class="text-base leading-[1.7] text-gray-300">
+      I create bugs for LLMs to solve. Buidling Random Stuff. Currently working
+      around Hono and MCPs.
+    </p>,
   );
 });
 
